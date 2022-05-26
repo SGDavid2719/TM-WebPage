cf7d6863b76627bbaa756c6129ca5a066cce08bf
SGDavid2719 <santome.galvan.david@gmail.com>
2022-05-26 23:32:17 +0200

    minor changes

--- dist/js/dishes.js
+++ dist/js/dishes.js
@@ -3,32 +3,6 @@ function createDishesCarousel(dishes) {
 
     let content = "";
 
-    // Indicators
-
-    // content +=
-    //     "<div class='carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4'>";
-
-    // for (dishIndex in dishes) {
-    //     if (dishIndex == 0)
-    //         content +=
-    //             "<button type='button' data-bs-target='#carouselCaptions' data-bs-slide-to='" +
-    //             dishIndex +
-    //             "' class='active' aria-current='true' aria-label='" +
-    //             "dish " +
-    //             dishIndex +
-    //             "'></button>";
-    //     else
-    //         content +=
-    //             "<button type='button' data-bs-target='#carouselCaptions' data-bs-slide-to='" +
-    //             dishIndex +
-    //             "' aria-current='true' aria-label='" +
-    //             "dish " +
-    //             dishIndex +
-    //             "'></button>";
-    // }
-
-    // content += "</div>";
-
     // Content
 
     content += "<div class='carousel-inner relative w-full overflow-hidden'>";
@@ -57,28 +31,6 @@ function createDishesCarousel(dishes) {
     }
 
     content += "</div>";
-
-    // // Previous slider
-
-    // content +=
-    //     "<button class='carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0' type='button' data-bs-target='#carouselCaptions' data-bs-slide='prev'>";
-
-    // content +=
-    //     "<span class='carousel-control-prev-icon inline-block bg-no-repeat' aria-hidden='true'></span>";
-    // content += "<span class='visually-hidden'>Previous</span>";
-
-    // content += "</button>";
-
-    // // Next sllider
-
-    // content +=
-    //     "<button class='carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0' type='button' data-bs-target='#carouselCaptions' data-bs-slide='next'>";
-
-    // content +=
-    //     "<span class='carousel-control-next-icon inline-block bg-no-repeat' aria-hidden='true'></span>";
-    // content += "<span class='visually-hidden'>Next</span>";
-
-    // content += "</button>";
 
     // Append content
 
